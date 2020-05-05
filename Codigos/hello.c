af82153c44a89e6486b1209541b15017b1054ad5
jonasBraga <jonasbraga2001@gmail.com>
2020-05-05 05:34:53 +0000

    square, rectangle and triangle Ok

--- Codigos/hello.c
+++ Codigos/hello.c
@@ -3,7 +3,7 @@
 int main(){
 
   printf("Para executar seu programa siga os seguintes passos: \n");
-  printf("\t» Execute: gcc -o exe ./codigos/hello.c && ./exe \n");
+  printf("\t» Execute: gcc -o exe ./Codigos/hello.c && ./exe \n");
   printf("\tsubstituindo hello.c pelo nome e endereço do seu arquivo. \n");
 
   return 0;
--- Codigos/lista_5/com110_lista5_2020000057.c
+++ Codigos/lista_5/com110_lista5_2020000057.c
@@ -4,6 +4,11 @@
 
 void showMenu();
 void drawSquare();
+void drawRectangle();
+void drawTriangle();
+void drawEquilateralTriangle();
+void drawDiamond();
+
 int main(){
   
   int opcao = 0;
@@ -17,20 +22,24 @@ int main(){
         drawSquare();
       break; 
       case 2:
-
+        drawRectangle();
       break;
       case 3:
+        drawTriangle();
       break;
       case 4:
+        drawEquilateralTriangle();
       break;
       case 5:
+        drawDiamond();
       break;
       case 6:
+        printf("Até mais!\n\n");
       break;
+      default:
+      printf("Opção nao encontrada!\n\n");
     }
   }
-
-
 
   return 0;
 }
@@ -47,7 +56,7 @@ void showMenu(){
   printf("\n##  2. Retângulo\t\t\t\t\t\t\t ##");
   printf("\n##  3. Triângulo Equilátero \t\t\t\t ##");
   printf("\n##  4. Triângulo Retângulo (catetos iguais)  ##");
-  printf("\n##  5. Outra \t\t\t\t\t\t\t\t ##");
+  printf("\n##  5. Losango\t\t\t\t\t\t\t\t ##");
   printf("\n##  6. Sair \t\t\t\t\t\t\t\t ##");
   printf("\n## \t\t\t\t\t\t\t\t\t\t\t ##");
   printf("\n###############################################");
@@ -59,15 +68,21 @@ void drawSquare(){
   int side = 0;
   printf("\nEntre com o lado do quadrado: ");
   scanf("%d", &side);
+  side += 2;
   for(int i = 1; i <= side; i++){
     for(int j = 1; j <= side; j++){
       if(i == 1 || i == side){
-          printf("_");
+          if(j == 1){
+            int border = ((side-2)*2 + 3);
+            while(border--){
+              printf("_");
+            }
+          }
       }else{
         if(j == 1 || j == side){
-          printf("|");          
+          printf("| ");          
         }else{
-          printf("@");
+          printf("@ ");
         }   
       }
     }  
@@ -75,4 +90,76 @@ void drawSquare(){
   }
 }
 
+void drawRectangle(){
+  int line = 0, column = 0;
+  printf("\nEntre com a base e a altura do retângulo: ");
+  scanf("%d %d", &column, &line);
+  line += 2;
+  column += 2;
+  for(int i = 1; i <= line; i++){
+    for(int j = 1; j <= column; j++){
+      if(i == 1 || i == line){
+          if(j == 1){
+            int border = ((column-2)*2 + 3);
+            while(border--){
+              printf("_");
+            }
+          }
+      }else{
+        if(j == 1 || j == column){
+          printf("| ");          
+        }else{
+          printf("@ ");
+        }   
+      }
+    }  
+    printf("\n");
+  }
+}
 
+void drawTriangle(){
+  int line = 0, column = 0;
+  printf("\nEntre com a altura do triângulo: ");
+  scanf("%d", &line);
+  line += 3;
+  for(int i = 1; i <= line; i++){
+    
+    int spaces = (line-i);
+    while(spaces--){
+      printf(" ");
+    }
+    if(i == 1){
+      printf(".");
+    }else if(i == 2){
+      printf("/ \\");
+    }else {
+
+      int j = 1;
+      while(j <= i){
+        if(j == 1){
+          if(i == line){
+            printf("/_");
+          }else{
+            printf("/ ");
+          }
+        }else if(j == i){
+          printf("\\");
+        }else if(i == line){
+          printf("__");
+        }else{
+          printf("@ ");
+        } 
+        j++;
+      }      
+    }
+    printf("\n");
+  }
+}
+
+void drawEquilateralTriangle(){
+
+}
+
+void drawDiamond(){
+
+}
