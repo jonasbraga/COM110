38b4f89fee0aee94f9fbc66c02008ad8966b52c0
jonasBraga <jonasbraga2001@gmail.com>
2020-05-06 00:52:01 +0000

    perimetro, area, triangulo retangulo e paralelogramo

--- Codigos/hello.c
+++ Codigos/hello.c
@@ -3,7 +3,7 @@
 int main(){
 
   printf("Para executar seu programa siga os seguintes passos: \n");
-  printf("\t» Execute: gcc -o exe ./Codigos/hello.c && ./exe \n");
+  printf("\t» Execute: gcc ./Codigos/hello.c -o exe -lm && ./exe \n");
   printf("\tsubstituindo hello.c pelo nome e endereço do seu arquivo. \n");
 
   return 0;
--- Codigos/lista_5/com110_lista5_2020000057.c
+++ Codigos/lista_5/com110_lista5_2020000057.c
@@ -6,8 +6,8 @@ void showMenu();
 void drawSquare();
 void drawRectangle();
 void drawTriangle();
-void drawEquilateralTriangle();
-void drawDiamond();
+void drawRectangleTriangle();
+void drawParallelogram();
 
 int main(){
   
@@ -28,10 +28,10 @@ int main(){
         drawTriangle();
       break;
       case 4:
-        drawEquilateralTriangle();
+        drawRectangleTriangle();
       break;
       case 5:
-        drawDiamond();
+        drawParallelogram();
       break;
       case 6:
         printf("Até mais!\n\n");
@@ -56,7 +56,7 @@ void showMenu(){
   printf("\n##  2. Retângulo\t\t\t\t\t\t\t ##");
   printf("\n##  3. Triângulo Equilátero \t\t\t\t ##");
   printf("\n##  4. Triângulo Retângulo (catetos iguais)  ##");
-  printf("\n##  5. Losango\t\t\t\t\t\t\t\t ##");
+  printf("\n##  5. Paralelogramo\t\t\t\t\t\t ##");
   printf("\n##  6. Sair \t\t\t\t\t\t\t\t ##");
   printf("\n## \t\t\t\t\t\t\t\t\t\t\t ##");
   printf("\n###############################################");
@@ -68,6 +68,10 @@ void drawSquare(){
   int side = 0;
   printf("\nEntre com o lado do quadrado: ");
   scanf("%d", &side);
+
+  printf("AREA: %d\n", side*side);  
+  printf("PERIMETRO: %d\n\n", side*4);  
+
   side += 2;
   for(int i = 1; i <= side; i++){
     for(int j = 1; j <= side; j++){
@@ -91,9 +95,14 @@ void drawSquare(){
 }
 
 void drawRectangle(){
+  
   int line = 0, column = 0;
   printf("\nEntre com a base e a altura do retângulo: ");
   scanf("%d %d", &column, &line);
+  
+  printf("AREA: %d\n", line*column);  
+  printf("PERIMETRO: %d\n\n", line*2+column*2);  
+  
   line += 2;
   column += 2;
   for(int i = 1; i <= line; i++){
@@ -118,9 +127,15 @@ void drawRectangle(){
 }
 
 void drawTriangle(){
-  int line = 0, column = 0;
-  printf("\nEntre com a altura do triângulo: ");
+  int line = 0;
+  printf("\nEntre com o lado do triângulo: ");
   scanf("%d", &line);
+
+  float area = (line*(sqrt(pow(line, 2) - pow(line/2.0, 2)))/2.0);
+  printf("AREA: %f\n", area);  
+  printf("PERIMETRO: %d\n\n", line*3);  
+  
+  
   line += 3;
   for(int i = 1; i <= line; i++){
     
@@ -156,10 +171,82 @@ void drawTriangle(){
   }
 }
 
-void drawEquilateralTriangle(){
+void drawRectangleTriangle(){
+  int line = 0;
+  printf("\nEntre com o cateto do triângulo: ");
+  scanf("%d", &line);
 
+  float area = (line * line) / 2;
+  printf("AREA: %f\n", area);  
+  printf("PERIMETRO: %f\n\n", line*2 + (sqrt(pow(line, 2) + pow(line, 2))));  
+  
+  line += 3;
+  for(int i = 1; i <= line; i++){
+    
+    int spaces = (line-i) * 2;
+    while(spaces--){
+      printf(" ");
+    }
+    if(i == 1){
+      printf(".");
+    }else if(i == 2){
+      printf("/ |");
+    }else {
+
+      int j = 1;
+      while(j <= i){
+        if(j == 1){
+          if(i == line){
+            printf("/_");
+          }else{
+            printf("/ ");
+          }
+        }else if(j == i){
+          printf("|");
+        }else if(i == line){
+          printf("__");
+        }else{
+          printf("@ ");
+        } 
+        j++;
+      }      
+    }
+    printf("\n");
+  }
 }
 
-void drawDiamond(){
+void drawParallelogram(){
+int line = 0, column = 0;
+  printf("\nEntre com a base e a altura do paralelogramo: ");
+  scanf("%d %d", &column, &line);
+  
+  printf("AREA: %d\n", line*column);  
+  printf("PERIMETRO: %d\n\n", line*2+column*2);  
+  
+  line += 2;
+  column += 2;
+  for(int i = 1; i <= line; i++){
 
+    int spaces = (line-i) * 2;
+    while(spaces--){
+      printf(" ");
+    }
+    for(int j = 1; j <= column; j++){
+      if(i == 1 || i == line){
+          if(j == 1){
+            int border = ((column-2)*2 + 3);
+            while(border--){
+              printf("_");
+            }
+          }
+      }else{
+        if(j == 1 || j == column){
+          printf("/ ");          
+        }else{
+          printf("@ ");
+        }   
+      }
+    }  
+    printf("\n");
+  }
 }
