91d5bfa1882468ed658187fe74903a4ea16a9875
jonasBraga <jonasbraga2001@gmail.com>
2020-05-19 01:31:04 +0000

    ex 3 e 4 lista 6 prontos

--- Codigos/lista_6/com110_lista6_ex03_2020000057.c
+++ Codigos/lista_6/com110_lista6_ex03_2020000057.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 int main()
 {
-  
+  int i = 0, diceNumbers[7] = {}, numbersDrawn[20] = {}, aux = 0, j = 0;
+  while(++i <= 20){
+    do{
+      printf("Entre com o %d numero: ", i);
+      scanf("%d", &aux);
+    }while(!(aux >= 1 && aux <= 6));
+    numbersDrawn[i] = aux;
+    diceNumbers[aux]++; 
+  }
+
+  while(++j <= 6){
+    printf("Numero %d foi sorteado %d vezes!\n", j, diceNumbers[j]);
+  }
+
   return 0;
 }
--- Codigos/lista_6/com110_lista6_ex04_2020000057.c
+++ Codigos/lista_6/com110_lista6_ex04_2020000057.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main()
 {
+  char texto[10] = {""};
+  int i = 10;
   
+  printf("\n Entre com o texto: ");
+  scanf("%[^\n]s", texto);
+  setbuf(stdin, NULL);
+  printf("\n Invertido: ");
+  
+  while(i--){
+    printf("%c", texto[i]);
+  }
+
+  printf("\n");
   return 0;
 }
