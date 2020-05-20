1fde4a7ad36b5ace321439129d5c4973d62de680
jonasBraga <50342952+jonasbraga@users.noreply.github.com>
2020-05-20 09:01:12 -0300

    updated lista 6 ex 01

--- Codigos/lista_6/com110_lista6_ex01_2020000057.c
+++ Codigos/lista_6/com110_lista6_ex01_2020000057.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#include <locale.h>
+
 int main()
 {
-  int i = 0, number = 0, numbers[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  while(++i <= 8){
-    number = 0;
-    printf("\nInsira o %d número: ", i);
-    scanf("%d", &number);
+    setlocale(LC_ALL, "portuguese");
     
-
-  }
-  return 0;
+    int vetor[7], i, j;
+    
+    printf("Entre com 8 valores para preencher um vetor: \n");
+    for(i=1; i<=8; i++){
+    	printf("Vetor[%d]: ", i);
+    	scanf("%d", &vetor[i]);
+    }
+    
+    for(i=1; i<=8; i++){
+        for(j=i+1; j<=8; j++){
+            if(vetor[i] > vetor[j]){
+                int x = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = x;
+            }
+        }
+    }
+    
+    printf("\nO vetor foi reorganizado em ordem crescente!");
+    for(i=1; i<=8; i++){
+        printf("\nVetor[%d]: %d", i, vetor[i]);
+    }
+    
+    return 0;
 }
